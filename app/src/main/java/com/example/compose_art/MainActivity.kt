f9403adee1609e68e67d7bbb3d657d732433a62e
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2025-02-08 17:26:18 +0500

    dayNight beach scene vector images completed

--- app/src/main/java/com/example/compose_art/MainActivity.kt
+++ app/src/main/java/com/example/compose_art/MainActivity.kt
@@ -5,16 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.compose_art.composables.BeachScene
-import com.example.compose_art.ui.theme.ComposeArtTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComposeArtTheme {
-                BeachScene()
-            }
+            BeachScene()
         }
     }
 }
