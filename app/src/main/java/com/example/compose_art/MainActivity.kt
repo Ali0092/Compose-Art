1ebc1072763387e8b059352450169bfe04b24247
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2025-02-11 22:12:23 +0500

    learned to draw triangles on canvas in compose

--- app/src/main/java/com/example/compose_art/MainActivity.kt
+++ app/src/main/java/com/example/compose_art/MainActivity.kt
@@ -12,15 +12,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.geometry.center
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.RadialGradientShader
 import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.compose_art.ui.theme.ComposeArtTheme
 
 val dayGradient = object : ShaderBrush() {
     override fun createShader(size: Size): Shader {
@@ -34,25 +38,142 @@ val dayGradient = object : ShaderBrush() {
 }
 
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyCanvas(modifier = Modifier.fillMaxSize())
+            TriangleCanvas(modifier = Modifier.fillMaxSize())
         }
+    }
+}
+
+
+@Composable
+fun TriangleCanvas(modifier: Modifier = Modifier) {
+    Canvas(modifier.background(dayGradient)) {
+        val trianglePath = Path().apply {
+            // Moves to top center position
+            val centerPoint = size.height/2-300f
+            moveTo(size.width / 2f, centerPoint)
+//            // Add line to bottom right corner
+            lineTo(size.width/2+200f, (size.height/2)+200f)
+//            // Add line to bottom left corner
+//            lineTo(size.width/2-200f, (size.height/2)+200f)
+        }
+        drawPath(trianglePath, color = Color.Black, style = Stroke(width = 8.dp.toPx(), cap = StrokeCap.Round))
     }
 }
 
 @Composable
 fun MyCanvas(modifier: Modifier = Modifier) {
     Canvas(modifier.background(dayGradient)) {
-        dra
+
+
+        drawRoundRect(
+            color = Color.Black,
+            size = Size(200f,200f),
+            cornerRadius = CornerRadius(12.dp.toPx(),12.dp.toPx()),
+            topLeft = calculateOffSet(size,200f, 200f),
+            style = Stroke(width = 5.dp.toPx(),
+            cap = StrokeCap.Round)
+        )
+
+        drawOval(
+            color = Color.Red,
+            size = Size(350f,400f),
+            style = Stroke(width = 5.dp.toPx(),
+            cap = StrokeCap.Round),
+            topLeft = calculateOffSet(size,350f, 400f),
+        )
+
+        drawRoundRect(
+            color = Color.Black,
+            size = Size(200f,200f),
+            cornerRadius = CornerRadius(12.dp.toPx(),12.dp.toPx()),
+            topLeft = Offset(0f,0f),
+            style = Stroke(width = 5.dp.toPx(),
+            cap = StrokeCap.Round)
+        )
+
+        drawRoundRect(
+            color = Color.Black,
+            size = Size(200f,200f),
+            cornerRadius = CornerRadius(12.dp.toPx(),12.dp.toPx()),
+            topLeft = Offset(size.width-200f,0f),
+            style = Stroke(width = 5.dp.toPx(),
+            cap = StrokeCap.Round)
+        )
+
+        drawRoundRect(
+            color = Color.Black,
+            size = Size(200f,200f),
+            cornerRadius = CornerRadius(12.dp.toPx(),12.dp.toPx()),
+            topLeft = Offset(0f,size.height-200f),
+            style = Stroke(width = 5.dp.toPx(),
+            cap = StrokeCap.Round)
+        )
+
+        drawRoundRect(
+            color = Color.Black,
+            size = Size(200f,200f),
+            cornerRadius = CornerRadius(12.dp.toPx(),12.dp.toPx()),
+            topLeft = Offset(size.width-200f,size.height-200f),
+            style = Stroke(width = 5.dp.toPx(),
+            cap = StrokeCap.Round)
+        )
+
+        drawRoundRect(
+            color = Color.Black,
+            size = Size(200f,200f),
+            cornerRadius = CornerRadius(12.dp.toPx(),12.dp.toPx()),
+            topLeft = Offset(size.width-200f,size.height-200f),
+            style = Stroke(width = 5.dp.toPx(),
+            cap = StrokeCap.Round)
+        )
+
+        drawRoundRect(
+            color = Color.Black,
+            size = Size(200f,200f),
+            cornerRadius = CornerRadius(12.dp.toPx(),12.dp.toPx()),
+            topLeft = Offset((size.width/2)-200f,(size.height/2)-200f),
+            style = Stroke(width = 5.dp.toPx(),
+            cap = StrokeCap.Round)
+        )
+
+        drawRoundRect(
+            color = Color.Black,
+            size = Size(200f,200f),
+            cornerRadius = CornerRadius(12.dp.toPx(),12.dp.toPx()),
+            topLeft = Offset((size.width/2)-200f,(size.height/2)),
+            style = Stroke(width = 5.dp.toPx(),
+            cap = StrokeCap.Round)
+        )
+
+        drawRoundRect(
+            color = Color.Black,
+            size = Size(200f,200f),
+            cornerRadius = CornerRadius(12.dp.toPx(),12.dp.toPx()),
+            topLeft = Offset((size.width/2),(size.height/2)-200f),
+            style = Stroke(width = 5.dp.toPx(),
+            cap = StrokeCap.Round)
+        )
+
+        drawRoundRect(
+            color = Color.Black,
+            size = Size(200f,200f),
+            cornerRadius = CornerRadius(12.dp.toPx(),12.dp.toPx()),
+            topLeft = Offset((size.width/2),(size.height/2)),
+            style = Stroke(width = 5.dp.toPx(),
+                cap = StrokeCap.Round)
+        )
+
     }
 }
 
-
+fun calculateOffSet( size: Size, rectAngleWidth: Float, rectAngleHeight: Float ): Offset {
+    return Offset((size.width/2)-(rectAngleWidth/2f),(size.height/2)-(rectAngleHeight/2f))
+}
 
 @Composable
 fun CirclePatters(modifier: Modifier = Modifier) {
