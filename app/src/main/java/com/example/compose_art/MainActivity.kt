de34c4dc7105d2785b5977ed8daa3e17e7a6f396
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2025-02-12 21:39:18 +0500

    tried basic hands drawing

--- app/src/main/java/com/example/compose_art/MainActivity.kt
+++ app/src/main/java/com/example/compose_art/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.compose_art
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,18 +10,19 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,27 +34,44 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Line(
+    val start: Offset, val end: Offset, val color: Color = Color.Black, val strokeWidth: Dp = 5.dp
+)
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun DrawingCanvas() {
 
-    val path = remember { Path() }
+    var points by remember { mutableStateOf<List<Offset>>(emptyList()) }
 
     Canvas(modifier = Modifier
         .fillMaxSize()
         .background(color = Color.White)
-        .pointerInput(true) {
-            detectDragGestures(onDrag = { pointerInputChange, dragAmount ->
-                path.quadraticBezierTo(
-                    pointerInputChange.position.x,
-                    pointerInputChange.position.y,
-                    (pointerInputChange.position.x + pointerInputChange.position.x) / 2,
-                    (pointerInputChange.position.y + pointerInputChange.position.y) / 2
+        .pointerInput(Unit) {
 
-                )
-            }, onDragStart = { offset ->
-                path.moveTo(offset.x, offset.y)
+            detectDragGestures(onDragStart = { touchPoint ->
+                points = listOf(touchPoint)
+            }, onDrag = { change, _ ->
+                val pointsFromHistory = change.historical
+                    .map { it.position }
+                    .toTypedArray()
+                val newPoints = listOf(*pointsFromHistory, change.position)
+                points = points + newPoints
             })
+
         }) {
-        drawPath(path, color = Color.Black, style = Stroke(width = 5f, cap = StrokeCap.Round, join = StrokeJoin.Round))
+        if (points.size > 1) {
+            val path = Path().apply {
+                val firstPoint = points.first()
+                val rest = points.subList(1, points.size - 1)
+
+                moveTo(firstPoint.x, firstPoint.y)
+                rest.forEach {
+                    lineTo(it.x, it.y)
+                }
+            }
+
+            drawPath(path, color = Color.Black, style = Stroke(width = 4.dp.toPx(), cap = StrokeCap.Round))
+        }
     }
 }
